9b3f8085960999d43b5ca52ef0974f81fc1869c7
Patrick Eitel <patrickeitel12@gmail.com>
2023-09-20 15:18:13 -0700

    Add a allTests.js, add a fetch request that will get all customers

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,1 +1,16 @@
 // FETCH REQUESTS
+
+export const getCustomers = 
+fetch('http://localhost:3001/api/v1/customers')
+.then((response) => {
+  if (!response.ok) {
+    throw new Error(`Fetch failure, STATUS CODE: ${response.status}`);
+  }
+  return response.json()
+})
+.catch((error) => {
+  console.error('Error fetching customers:', error)
+  throw error
+});
+
+
