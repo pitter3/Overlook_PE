118bcaf8423d618846b5e05c49dcbda53f47aa54
Patrick Eitel <patrickeitel12@gmail.com>
2023-09-24 15:58:19 -0700

    Added lodash package, added functions to add properties to bookings and currentUser

--- src/scripts.js
+++ src/scripts.js
@@ -1,31 +1,52 @@
 import './css/styles.css';
 import './images/turing-logo.png'
-import { 
-  getCustomers, 
-  getRooms, 
-  getSingleCustomer, 
-  getBookings, 
-  postBooking, 
-  deleteBooking,
-  getBookingsByID,
+import {orderBy} from 'lodash'
+import {
+    getCustomers,
+    getRooms,
+    getSingleCustomer,
+    getBookings,
+    postBooking,
+    deleteBooking,
+    getBookingsByID,
 } from './apiCalls.js';
-import { getCustomerID, findTotalSpent, findBookings } from './functions';
+import {getCustomerID, findTotalSpent, findBookings} from './functions';
 
 
 let activeCustomer = {};
 
 let customers = null;
 let rooms = null;
-let bookings = null
+let bookings = null;
 
 Promise.all([getCustomers, getRooms, getBookings])
-.then(([customersData, roomsData, bookingsData]) => {
-  customers = customersData.customers;
-  rooms = roomsData.rooms;
-  bookings = bookingsData.bookings;
-});
+    .then(([customersData, roomsData, bookingsData]) => {
+        customers = customersData.customers;
+        rooms = roomsData.rooms;
+        bookings = mergeBookingsWithRooms(bookingsData.bookings, roomsData.rooms)
+    });
 
 
+const mergeBookingsWithRooms = (bookings = [], rooms = []) => bookings.map((booking) => ({
+    ...booking,
+    dateObject: new Date(booking.date),
+    room: rooms.find((room) => room.number === booking.roomNumber)
+    }))
+
+const splitBookingsByPastAndUpcoming = (bookings = []) => {
+    const today = new Date()
+    today.setHours(0)
+    today.setMinutes(0)
+    today.setMilliseconds(0)
+
+    const pastBookings = bookings.filter((booking) => booking.dateObject < today)
+    const upcomingBookings = bookings.filter((booking) => booking.dateObject > today)
+
+    return {
+        pastBookings: orderBy(pastBookings, ['dateObject'], ['desc']),
+        upcomingBookings: orderBy(upcomingBookings, ['dateObject'], ['desc']),
+    }
+}
 
 // QUERY SELECTORS
 
@@ -34,62 +55,65 @@ const usernameField = document.getElementById("username-input");
 const passwordField = document.getElementById("password-input");
 const loginContainer = document.querySelector(".login-container");
 
-loginForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  
-  if (checkUsername(usernameField.value) && checkPassword(passwordField.value)) {
-    console.log("LOGIN SUCCESS!!!")
-    loginContainer.remove()
-    const customerID = getCustomerID(usernameField.value);
-    const customerBookings = findBookings(customerID, bookings)
-      activeCustomer.name = getCustomerName(customerID);
-      activeCustomer.id = customerID
-      activeCustomer.bookings = customerBookings
-      activeCustomer.totalSpent = findTotalSpent(rooms, activeCustomer.bookings)
-      console.log(activeCustomer);
-    // renderUserPage()
-    // populate the main page based on the user who logged in
-    // display total spent on rooms
-    // display current (and past) bookings
-  } else {
-    console.log("LOGIN FAILURE :(")
-  }
-  clearLoginFields();
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (checkUsername(usernameField.value) && checkPassword(passwordField.value)) {
+        console.log("LOGIN SUCCESS!!!")
+        loginContainer.remove()
+        const customerID = getCustomerID(usernameField.value);
+        const customerBookings = findBookings(customerID, bookings)
+        activeCustomer.name = getCustomerName(customerID);
+        activeCustomer.id = customerID
+        activeCustomer.bookings = customerBookings
+        activeCustomer.totalSpent = findTotalSpent(rooms, activeCustomer.bookings)
+        activeCustomer.pastAndUpcomingBookings = splitBookingsByPastAndUpcoming(activeCustomer.bookings)
+        console.log(activeCustomer);
+        console.log(bookings)
+        console.log(rooms)
+        // renderUserPage()
+        // populate the main page based on the user who logged in
+        // display total spent on rooms
+        // display current (and past) bookings
+    } else {
+        console.log("LOGIN FAILURE :(")
+    }
+    clearLoginFields();
 });
 
 // DOM FUNCTIONS
 
 function clearLoginFields() {
-  usernameField.value = "";
-  passwordField.value = "";
+    usernameField.value = "";
+    passwordField.value = "";
 };
 
 const checkUsername = (username) => {
-  const match = username.match(/^customer([1-9]|[1-4][0-9]|50)$/);
-  if (match) {
-    return true;
-  }
-  console.log("INVALID USERNAME");
-  return false;
+    const match = username.match(/^customer([1-9]|[1-4][0-9]|50)$/);
+    if (match) {
+        return true;
+    }
+    console.log("INVALID USERNAME");
+    return false;
 };
 
 const checkPassword = (password) => {
-  if (password === "overlook2021") {
-    return true 
-  } else {
-    console.log("WRONG PASSWORD")
-    return false 
-  }
+    if (password === "overlook2021") {
+        return true
+    } else {
+        console.log("WRONG PASSWORD")
+        return false
+    }
 };
 
 function getCustomerName(id) {
-  const customer = customers.find((customer) => {
-    return customer.id === id;
-  });
-  return customer.name;
+    const customer = customers.find((customer) => {
+        return customer.id === id;
+    });
+    return customer.name;
 }
 
 // function getRoomNumbers(customerBookings) {
 //   const roomNumbers = customerBookings.map((booking) => booking.roomNumber);
 //   return roomNumbers;
-// }
+// }bo
